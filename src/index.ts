3acfef04e5f40e609d62436e5e342a0df09ed703
Nicolas Pope <nicolas.pope@uef.fi>
2025-07-28 09:37:18 +0300

    Allow static hosting

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,45 @@ import 'dotenv/config';
 import express from 'express';
 import { ExpressPeerServer } from 'peer';
 import { logger } from './logger.js';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import path from 'path';
+
+const argv = yargs(hideBin(process.argv))
+    .option('port', {
+        alias: 'p',
+        type: 'number',
+        description: 'Port for the PeerServer to listen on',
+        default: 9000,
+    })
+    .option('key', {
+        alias: 'k',
+        type: 'string',
+        description: 'Key for the PeerServer',
+        default: 'genaikey',
+    })
+    .options('files', {
+        alias: 'f',
+        type: 'string',
+        description: 'Path to the files directory',
+        default: '',
+    })
+    .help()
+    .alias('help', 'h')
+    .parseSync();
 
 import { IRoutedData, registerClient, removeClient, routeData } from './dataRouter.js';
 
 const app = express();
 
-app.get('/', (_, res) => {
-    res.sendStatus(403);
-});
+if (argv.files !== '') {
+    app.use(express.static(argv.files));
+}
 
-const server = app.listen(parseInt(process.env.PEER_PORT ?? '9000'));
+const server = app.listen(argv.port);
 
 const peerServer = ExpressPeerServer(server, {
-    key: process.env.PEER_KEY || 'genaikey',
+    key: argv.key,
 });
 
 app.use(peerServer);
@@ -61,52 +87,6 @@ app.get('/rtcconfig', async (_, res) => {
     res.end();
 });
 
-/*app.get('/model/:code/:component', async (req, res) => {
-    const { code, component } = req.params;
-    const pwd = req.query.pwd as string;
-
-    try {
-        const ws = await doSignaling(peer, id, `tm-${code}`);
-
-        try {
-            const content = await makeDataRequest(peer, component, pwd);
-
-            ws.close();
-            peer.close();
-
-            res.setHeader('Access-Control-Allow-Origin', '*');
-
-            if (content) {
-                if (content.event === 'project') {
-                    res.contentType('application/zip');
-                    res.send(Buffer.from(content.project, 'base64'));
-                } else if (content.component === 'model') {
-                    res.contentType('application/json');
-                    res.send(content.data);
-                } else if (content.component === 'metadata') {
-                    res.contentType('application/json');
-                    res.send(content.data);
-                } else if (content.component === 'weights') {
-                    res.contentType('application/octet-stream');
-                    res.send(Buffer.from(content.data));
-                } else {
-                    res.status(404);
-                    console.error('Content was not found', content);
-                    res.end();
-                }
-            } else {
-                res.status(400);
-                res.end();
-            }
-        } catch (e) {
-            ws.close();
-            console.error(e);
-            console.error('Peer was not found');
-            res.status(404);
-            res.end();
-        }
-    } catch (e) {
-        res.status(500);
-        res.end();
-    }
-});*/
+app.get('/*path', (_, res) => {
+    res.sendFile(path.resolve(argv.files, 'index.html'));
+});
