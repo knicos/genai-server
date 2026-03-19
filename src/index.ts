fb140173be17eccae3bd7b5adfdd95f624eb8c51
Nicolas Pope <nicolas.pope@uef.fi>
2026-03-19 07:08:23 +0200

    Add TM model sharing

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { logger } from './logger.js';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import path from 'path';
+import jszip from 'jszip';
 
 const argv = yargs(hideBin(process.argv))
     .option('port', {
@@ -86,6 +87,88 @@ app.get('/rtcconfig', async (_, res) => {
     res.send(config);
     res.end();
 });
+
+/* Model sharing */
+
+// This should be more robust
+const MODELS = new Map<string, Buffer>();
+
+app.post('/model/:code', express.raw({ type: 'application/zip', limit: '20mb' }), async (req, res) => {
+    const { code } = req.params;
+
+    // Validate and secure this endpoint
+
+    if (!req.body || !Buffer.isBuffer(req.body)) {
+        res.status(400).send('Invalid model data');
+        return;
+    }
+
+    MODELS.set(code, req.body);
+    res.sendStatus(200);
+});
+
+app.delete('/model/:code', async (req, res) => {
+    const { code } = req.params;
+    MODELS.delete(code);
+    res.sendStatus(200);
+});
+
+app.options('/model/:code', (req, res) => {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.sendStatus(204);
+});
+
+app.get('/model/:code/:component', async (req, res) => {
+    const { code, component } = req.params;
+
+    const modelZip = MODELS.get(code);
+    if (!modelZip) {
+        res.sendStatus(404);
+        return;
+    }
+
+    if (!Buffer.isBuffer(modelZip)) {
+        res.sendStatus(500);
+        return;
+    }
+
+    const zip = await jszip.loadAsync(modelZip);
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    if (component === 'project.zip') {
+        res.contentType('application/zip');
+        res.send(modelZip);
+        return;
+    }
+
+    if (!zip.files[component]) {
+        res.sendStatus(404);
+        return;
+    }
+
+    const contentData = await zip.files[component].async('nodebuffer');
+
+    switch (component) {
+        case 'model.json':
+            res.contentType('application/json');
+            res.send(contentData);
+            break;
+        case 'metadata.json':
+            res.contentType('application/json');
+            res.send(contentData);
+            break;
+        case 'weights.bin':
+            res.contentType('application/octet-stream');
+            res.send(contentData);
+            break;
+        default:
+            res.sendStatus(404);
+            break;
+    }
+});
+
+/* Fallback */
 
 app.get('/*path', (_, res) => {
     res.sendFile(path.resolve(argv.files, 'index.html'));
